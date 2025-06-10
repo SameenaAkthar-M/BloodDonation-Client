e2a556eb6e6362c7530e4e2ecb276bc6ba5ea5ad
SameenaAkthar <sameenaaktharm@gmail.com>
2025-06-10 22:29:18 +0530

    commi

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://blooddonation-server-1.onrender.com',
         changeOrigin: true,
       },
     },
